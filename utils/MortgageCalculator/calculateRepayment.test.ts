38d58e1949e111625597ae4d0db01a1a0915ea3f
Jeffrey Lake <jlake.jobs@gmail.com>
2024-04-07 18:15:04 +0400

    add bounds checks

--- utils/MortgageCalculator/calculateRepayment.test.ts
+++ utils/MortgageCalculator/calculateRepayment.test.ts
@@ -15,4 +15,41 @@ describe("calculateMonthlyPayment", () => {
     const result = calculateMonthlyPayment(300000, 60000, 3.5, 15);
     expect(result).toBeCloseTo(1715.72, 2);
   });
+
+  test("should throw an error when the property price is less than 0", () => {
+    expect(() => {
+      calculateMonthlyPayment(0, 60000, 3.5, 15);
+    }).toThrow("Property Price should be greater than 0");
+  });
+
+  test("should throw an error when the deposit is less than 0", () => {
+    expect(() => {
+      calculateMonthlyPayment(300000, -5000, 3.5, 15);
+    }).toThrow("Deposit should be 0 or higher");
+  });
+
+  test("should throw an error when the deposit is greater than the property price", () => {
+    expect(() => {
+      calculateMonthlyPayment(300000, 500000, 3.5, 15);
+    }).toThrow("Deposit should be less than Property Price");
+  });
+
+  test("should throw an error when the annual interest rate is less than 0", () => {
+    expect(() => {
+      calculateMonthlyPayment(300000, 60000, -3.5, 15);
+    }).toThrow("Annual Interest Rate should be higher than 0");
+  });
+
+  test("should throw an error when the annual interest rate is greater than 100", () => {
+    expect(() => {
+      calculateMonthlyPayment(300000, 60000, 103.5, 15);
+    }).toThrow("Annual Interest Rate should be less than 100");
+  });
+
+  test("should throw an error when the mortgage term in years is less than 0", () => {
+    expect(() => {
+      calculateMonthlyPayment(300000, 60000, 3.5, -15);
+    }).toThrow("Mortgage Term in Years should be more than 0");
+  });
+
 });
--- utils/MortgageCalculator/calculateRepayment.ts
+++ utils/MortgageCalculator/calculateRepayment.ts
@@ -13,6 +13,14 @@ export function calculateMonthlyPayment(
   annualInterestRate: number,
   mortgageTermInYears: number
 ): number {
+
+  if (propertyPrice <= 0) throw new Error("Property Price should be greater than 0");
+  if (deposit < 0) throw new Error("Deposit should be 0 or higher");
+  if (deposit > propertyPrice) throw new Error("Deposit should be less than Property Price");
+  if (annualInterestRate < 0) throw new Error("Annual Interest Rate should be higher than 0");
+  if (annualInterestRate > 100) throw new Error("Annual Interest Rate should be less than 100");
+  if (mortgageTermInYears <= 0) throw new Error("Mortgage Term in Years should be more than 0");
+
   const adjustedLoanAmount = propertyPrice - deposit;
   const monthlyInterestRate = annualInterestRate / 100 / 12;
   const numberOfPayments = mortgageTermInYears * 12;
